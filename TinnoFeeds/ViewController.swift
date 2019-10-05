d24c0c9d691e00ac11a552d9216cf10145af9362
John Wong <John@values1.com>
2019-10-06 01:23:03 +0800

    Initial Commit

---
+++ TinnoFeeds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinnoFeeds
+//
+//  Created by John Wong Chon Yong on 06/10/2019.
+//  Copyright © 2019 TinnoLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
